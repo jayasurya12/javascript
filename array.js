2eeca6f31541f8715f76a5bc844cacbb21c9c9a7
unknown <jayasurya12vj@gmail.com>
2020-08-10 12:41:18 +0530

    array.js

---
+++ array.js
@@ -0,0 +1,11 @@
+/////////////////
+///////////////
+///////////////
+let array=[2,4,5,6]
+var min= Math.min(...array)
+var max= Math.max(...array)
+for(let i=min;i<=max;i++){
+   if(array.includes(i)===false){
+      console.log(i)
+   }
+}
